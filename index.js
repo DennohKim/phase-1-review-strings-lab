cdb7b55cd82b0c31f204740efa9d8b1475401125
Dennis Kimathi <kimathidennis38@gmail.com>
2022-06-06 16:31:51 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Write your code in this file!
+//Current User variable
+let currentUser = "Dennis Kimathi";
+
+let welcomeMessage = "Welcome to Flatbook, ";
+
+currentUser = welcomeMessage;
+
+console.log(currentUser)
+
+welcomeMessage =  `${currentUser}!`
+
+//Excited Welcome Message Variable
+
+let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ';
+
+let upperCaseCurrentUser = currentUser.toUpperCase();
+
+excitedWelcomeMessage = `${upperCaseCurrentUser}!`
+
+//Short Greeting variable
+let shortGreeting = 'Welcome, ';
+
+const firstInitial = currentUser[0];
+const restOfName = currentUser.slice(1);
+
+console.log(firstInitial);
+console.log(restOfName)
+
+
+shortGreeting = `${shortGreeting}!`
